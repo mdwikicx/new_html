3915bb51d7703cd4f5910f6521bd90279fe27c1a
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-04 11:10:51 +0300

    .

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,7 +12,7 @@ if (file_exists($vendor_path)) {
     require $vendor_path;
 };
 
-require __DIR__ . '/../src/new_html_src/require.php';
+require __DIR__ . '/../src/bootstrap.php';
 
 use PHPUnit\Framework\TestCase;
 
