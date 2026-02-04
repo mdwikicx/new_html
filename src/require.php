13814a2f2571a750c8e92658bf8e45ee9f426591
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-05 01:59:18 +0300

    Update require.php

--- src/require.php
+++ src/require.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Autoloader and initialization file for the new_html application
  *
@@ -12,6 +13,14 @@ if (defined('DEBUGX') && DEBUGX === true) {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
+}
+
+if (!defined('APP_ROOT')) {
+    define('APP_ROOT', dirname(__DIR__));
+}
+
+if (!defined('REVISIONS_PATH')) {
+    define('REVISIONS_PATH', APP_ROOT . '/../revisions_new');
 }
 
 include_once __DIR__ . '/new_html_src/require.php';
