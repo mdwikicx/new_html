648791294c2e65f9282cb335caad42d4fd4e125d
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-04 07:04:30 +0300

    Update lead_section.php

--- new_html_src/WikiParse/lead_section.php
+++ new_html_src/WikiParse/lead_section.php
@@ -5,6 +5,27 @@ namespace Lead;
 use function Lead\get_lead_section;
 */
 
+function get_lead_section_old($wikitext)
+{
+    if (empty($wikitext) || strpos($wikitext, '==') === false) {
+        return $wikitext;
+    }
+
+    // Split the wikitext into sections by lines starting with ==+ and get only the first section
+    $sections = preg_split('/==+/', $wikitext, 2, PREG_SPLIT_NO_EMPTY);
+    $lead = $sections[0] ?? '';
+
+    if (empty($lead)) {
+        return $wikitext;
+    }
+
+    $lead .= "\n==References==\n<references />";
+
+    $lead = refs_expend_work($lead, $wikitext);
+
+    return $lead;
+}
+
 function get_lead_section($wikitext)
 {
     if (empty($wikitext)) {
