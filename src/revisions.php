2bf3e1e86142cafc9cb02df254a096b8796b86cf
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-05 06:14:07 +0300

    1

--- src/revisions.php
+++ src/revisions.php
@@ -13,15 +13,14 @@
 <html lang="en">
 
 <?php
-define('DEBUGX', getenv('APP_DEBUG') === '1'); // Set APP_DEBUG=1 in development
+// Use modern PSR-4 autoloading
+require_once __DIR__ . "/bootstrap.php";
 
 if (defined('DEBUGX') && DEBUGX === true) {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 }
-// Use modern PSR-4 autoloading
-require_once __DIR__ . "/bootstrap.php";
 
 use function MDWiki\NewHtml\Application\Controllers\get_Data;
 use function MDWiki\NewHtml\Application\Controllers\dump_both_data;
