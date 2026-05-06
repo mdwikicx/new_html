1629a0949e1893be0ebc29c5d65bce136d00c961
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-05-07 02:13:32 +0300

    Update HtmlToSegmentsService.php

--- src/Services/Html/HtmlToSegmentsService.php
+++ src/Services/Html/HtmlToSegmentsService.php
@@ -34,6 +34,7 @@ function do_html_to_seg(string $text): string
     // $result = str_replace("https://medwiki.toolforge.org/wiki/", "https://en.wikipedia.org/wiki/", $result);
 
     if ($result == 'Content for translate is not given or is empty') return "";
+    if ($result == 'Sectionwrap: Attempting to remove a non-section tag: undefined') return "";
     return $result;
 }
 
